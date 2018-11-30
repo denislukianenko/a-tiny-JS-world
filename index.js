34db313f338d174b3393d3b561b8184648d95b24
Денис Лук'яненко <denlyk1@gmail.com>
2018-11-30 16:00:19 +0200

    Second try

--- index.js
+++ index.js
@@ -7,9 +7,76 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
-
+const dog = {
+  species: "dog",
+  name: "Toby",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "woof-woof!",
+  friends: ["Tihon"]
+};
+const cat = {
+  species: "cat",
+  name: "Tihon",
+  gender: "male",
+  legs: 4,
+  hands: 0,
+  saying: "meow-meow!",
+  friends: ["Alya"]
+};
+const woman = {
+  species: "human",
+  name: "Alya",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: "Hello, there!",
+  friends: ["Tihon", "Toby"]
+};
+const man = {
+  species: "human",
+  name: "Alex",
+  gender: "male",
+  legs: 2,
+  hands: 2,
+  saying: "What's up!",
+  friends: ["Toby"]
+};
+const catWoman = {
+  species: "cat-woman",
+  name: "Ann",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: cat.saying,
+  friends: ["Alya"]
+};
+function printBeing(obj) {
+  print(
+    obj.species +
+      ";" +
+      obj.name +
+      ";" +
+      obj.gender +
+      ";" +
+      obj.legs +
+      ";" +
+      obj.hands +
+      ";" +
+      obj.saying +
+      ";" +
+      obj.friends
+  );
+}
+// ... other print-outs ...
 
 // ======== OUTPUT ========
+printBeing(dog);
+printBeing(cat);
+printBeing(woman);
+printBeing(man);
+printBeing(catWoman);
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
@@ -27,5 +94,3 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
